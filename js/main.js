6d4cf81d5a49758780eab6702ccd27a5649f616c
WRQQQ <wrqqqrrr@rambler.ru>
2016-11-20 21:36:04 +0300

    Styles, complete, fixing edit time

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 
 var x = document.getElementById("input");
 var b = document.getElementById("btn");
-var ul = document.createElement('ul');
-var cont = document.getElementById("content"); 
-var inp = document.createElement('input');
+var ul = document.createElement("ul");
+var cont = document.querySelector(".content"); 
+var inp = document.createElement("input");
+var tasks = ["Погулять по Москве", "Покурить", "Написать ToDo", "Поймать рыбу", "Выпить чай", "Нарисовать картину", "Заняться делом", "Завоевать мир"];
 
+function getTask (array) {
+	function getRandomArbitary(min, max) {
+  		return Math.random() * (max - min) + min;
+	}
+	var maxInt = array.length;
+	var taskIndex =  Math.floor(getRandomArbitary(0, maxInt));
+	var task = array[taskIndex];
+	return task;
+}
+var arrTask = getTask(tasks);
+function insertTask () {
+	var elemTask = document.querySelector('#taskArr');
+	elemTask.innerHTML = 'Например, ' + arrTask;
+}
+insertTask();
 
-
-var validate = function(inputName, button) {
+function validate(inputName, button) {
 	incorrect = false;
 	var value = inputName.value.trim();
 	if (!value) {
@@ -20,7 +35,11 @@ var validate = function(inputName, button) {
 		}, 1100);
 	}
 }
-
+function getTime () {
+			var now = new Date(); 
+			var time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+			return time;
+}
 
 b.addEventListener('click', function() {
 	validate(x, this);
@@ -31,16 +50,17 @@ b.addEventListener('click', function() {
 	edit.className = 'edit';
 	var del = document.createElement('button');
 	del.className = 'del';
+	var complete = document.createElement('button');
+	complete.className = 'complete';
 	var editInp = document.createElement('input');
 	editInp.className = 'editInp';
-	edit.innerHTML = 'Edit';
-	del.innerHTML = 'Delete';
-	var now = new Date(); 
-	var time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+	var time = getTime();
 	var li = document.createElement('li');
-	li.innerHTML = "<span>" + x.value + "    " + time + "</span>";
+	li.className = "animated fadeInUp";
+	li.innerHTML = "<span class='time'>" + time + "</span>" + "<div class='text'>" + "<span class='task'>" + x.value + "</span>" + "</div>";
 	li.appendChild(edit);
 	li.appendChild(del);
+	li.appendChild(complete);
 	ul.appendChild(li);
 	cont.appendChild(ul);
 	x.value = '';
@@ -51,19 +71,31 @@ b.addEventListener('click', function() {
 	function hasClass(elem, className) {
     return elem.className.split(' ').indexOf(className) > -1;
 }
+  editInp.oninput = function() {
+    edit.style.display = 'inline-block';
+    edit.style.opacity = '1';
+  };
+  editInp.onblur = function() {
+  	setTimeout(function() {
+  		edit.style.display = 'none';	
+	}, 300);
+  	
+  }
 li.addEventListener('click', function (e) {
     if (hasClass(e.target, 'edit')) {
     	inpet = this.getElementsByTagName('input')[0];
-    	console.log(inpet)
 		validate(inpet, e.target);
 			if (incorrect) {
 				return;
 			}
-			this.firstChild.innerHTML = inpet.value;
+			this.querySelector('.text').innerHTML = inpet.value;
 			inpet.value = '';
+			var taskTime = getTime();
+			this.querySelector('.time').innerHTML = taskTime;
     } else if (hasClass(e.target, 'del')) {
-    	console.log(this);
-        this.remove();
+    	this.remove();	
+    } else if (hasClass(e.target, 'complete')) {
+    	this.className = "complete_task";
     }
 }, false);
 
